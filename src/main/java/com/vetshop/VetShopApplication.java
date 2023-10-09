f4530481a21351608a7e8dd7348516daff64c3a3
keren <keren@MSI>
2023-10-09 17:11:51 -0600

    Primer Commit del proyecton VetShop

---
+++ src/main/java/com/vetshop/VetShopApplication.java
@@ -0,0 +1,13 @@
+package com.vetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetshop/VetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
